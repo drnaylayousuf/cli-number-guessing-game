a266916d1fa4702e0e46409bea48bb4467b9d850
aliayousuf <160315316+aliayousuf@users.noreply.github.com>
2024-03-23 15:29:35 +0500

    change

---
+++ main.ts
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+
+
+
+
+const myRandomNumber = Math.floor(Math.random() * 6 + 1);
+
+console.log(chalk.blueBright("WELCOME TO THE NUMBER GUESSING GAME"));
+
+const answers = await inquirer.prompt([
+
+    {
+         name: "guessinggame",
+         type: "number",
+         message: "Enter your guess number from 1-6",
+
+    }
+]);
+
+if (answers.guessinggame === myRandomNumber) {
+
+    console.log(chalk.greenBright("congrats your guess is correct you have won $20"));
+    
+}
+else{
+    console.log(chalk.redBright("Opps you lost the Game"));
+    
+}
